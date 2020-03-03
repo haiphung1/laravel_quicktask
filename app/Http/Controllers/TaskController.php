08b452359460100c005d6775d82199a9773c3c47
haiphung1 <haipmph05501@fpt.edu.vn>
2020-03-03 13:44:44 +0700

    create and delete

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
 use Illuminate\Http\Request;
 use App\Models\Task;
 
@@ -12,5 +13,21 @@ class TaskController extends Controller
         $tasks = Task::all();
 
         return view('index', compact('tasks'));
+    }
+
+    public function store(TaskRequest $request)
+    {
+        $task = new Task();
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return redirect()->route('tasks.index');
     }
 }
